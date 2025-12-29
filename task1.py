1b1a733997a0c02d43443b60b0db2632224bf324
Daksh Tekwani <dakshtekwani2504@gmail.com>
2025-12-29 12:39:13 +0530

    Update task1.py

--- task1.py
+++ task1.py
@@ -7,3 +7,18 @@ try:
     
 except FileNotFoundError:
     print("Error: The file 'sample.txt' was not found.")
+
+
+
+#Sample Output (File exists)
+'''
+Line1: This is a sample text file.
+Line2: It contains multiple lines.
+Line3: Bye
+Line4: Have a good day
+'''
+
+#Sample Output (File doesn not exist)
+'''
+Error: The file 'sample.txt' was not found.
+'''
